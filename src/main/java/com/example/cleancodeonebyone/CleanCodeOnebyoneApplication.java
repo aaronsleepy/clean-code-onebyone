b6ea46bb9f7b6f19455a1a3f483ff86ffa80b516
aaronsleepy <aaron@kmong.com>
2022-02-15 09:57:49 +0900

    Initialized new project

---
+++ src/main/java/com/example/cleancodeonebyone/CleanCodeOnebyoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleancodeonebyone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeOnebyoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanCodeOnebyoneApplication.class, args);
+    }
+
+}
